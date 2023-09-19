42a8a37d1b1bb56ec258be6cff8036efe1df83ac
voary-jea <voaryrabarijoelina@gmil.com>
2023-09-19 17:40:39 +0300

    projet22

---
+++ ggeo.php
@@ -0,0 +1,10 @@
+<?php
+    $ville = tana;
+    if ($ville == tana) {
+        echo 'jolie'; 
+    } else {'moche';
+    }
+    if ($ville != tana) {
+        echo 'meta';
+    } else {'magm';
+    }
