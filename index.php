8ed7105f3af8764c336120ff6873395fb329ccc4
voary-jea <voaryrabarijoelina@gmil.com>
2023-09-19 17:08:45 +0300

    new project

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+  $true = 1;
+  $false = 0;
+    if ($true == 1) {
+      echo 'voary';
+    }
+    if ($true != 1) {
+      echo 'je';
+    }
+    if ($false == 0) {
+      echo 'francia';
+    } else {'jeannie';
+    }
+ 
